15f423c28f8e42249a3f9e9c38fe4e8645035a73
Lay Sukmin Lim <imsukmin@gmail.com>
2014-05-13 01:33:35 +0900

    add function

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
-var config = require('./config'); // make yours if you don't make it. it will not work.
-var bodyParser = require('body-parser');
+var config = require('./config');
+var bodyParser = require('body-parser');var bodyParser = require('body-parser');
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+
+// connect to Database
+var client = mysql.createConnection({
+	user: config.db.host,
+	password: config.db.password,
+	database: config.db.dbname	// instead of "client.query('USE [DBname]')"
+});
 
 app.use(bodyParser()); // pull information from html in POST
 app.use(bodyParser.json());
@@ -28,33 +35,21 @@ app.get('/connect', function(req, res){
 
 });
 
-app.get('/nfc', function(req, res){
+app.get('/GetByNFC', function(req, res){
 	var id = req.query.id;
 	var name = req.query.name;
 
 	res.send('I get NFC id No. ' + id + " and name is " + name);
-	console.log('nfc Worked');
+	console.log('GetByNFC Worked');
 });
 
-app.get('/ap', function(req, res){
-	var level = req.query.level;	
-	res.send('I get AP Level [ ' + level + ' ] ');
-	console.log('ap Worked');
-});
-
-app.get('/json', function(req, res){
+app.get('/getAP', function(req, res){
 	res.set('Content-Type', 'text/html');
 
-	var query = 'select * from BoothInfo';
+	var query = 'select * from AP';
 	if(req.query.q != null){
 		query = req.query.q;
 	}
-
-	var client = mysql.createConnection({
-		user: config.db.host,
-		password: config.db.password,
-		database: config.db.dbname	// instead of "client.query('USE [DBname]')"
-	});
 
 	client.query(query, function ( error, result, fields ){
 		if(error){
@@ -62,9 +57,67 @@ app.get('/json', function(req, res){
 		} else {
 			res.send(result);
 		}
-		console.log('json Worked [' + req.query.q + ']');
+		console.log('getAP Worked');
 	})
-	
+});
+
+app.get('/ExhibitionInfo', function(req, res){
+	res.set('Content-Type', 'text/html');
+
+	var query = 'select * from ExhibitionInfo';
+
+	client.query(query, function ( error, result, fields ){
+	if(error){
+		res.send('query is not correct! query is ' + query + ' and error is ' + error);
+	} else {
+		res.send(result);
+	}
+		console.log('ExhibitionInfo Worked [' + req.query.q + ']');
+	})
+});
+
+app.get('/BoothInfo', function(req, res){
+	res.set('Content-Type', 'text/html');
+
+	var query = 'select * from BoothInfo';
+
+	client.query(query, function ( error, result, fields ){
+		if(error){
+			res.send('query is not correct! query is ' + query + ' and error is ' + error);
+		} else {
+			res.send(result);
+		}
+		console.log('BoothInfo Worked [' + req.query.q + ']');
+	})
+});
+app.get('/BoothList', function(req, res){
+	res.set('Content-Type', 'text/html');
+
+	var query = 'select `index`, `teamName`, `nfcTagId` from BoothInfo';
+
+	client.query(query, function ( error, result, fields ){
+		if(error){
+			res.send('query is not correct! query is ' + query + ' and error is ' + error);
+		} else {
+			res.send(result);
+		}
+		console.log('BoothList Sended');
+	})
+});
+
+app.get('/admin/query', function(req, res){
+	res.set('Content-Type', 'text/html');
+
+	var query = req.query.query;
+
+	client.query(query, function ( error, result, fields ){
+		if(error){
+			res.send('query is not correct! query is ' + query + ' and error is ' + error);
+		} else {
+			res.send(result);
+		}
+		console.log('Admin-query worked');
+	})
 });
 
 app.get('/200', function(req, res){
