9ca32e92db189307a03ae8eed0cdb2ccb6aa97ea
dokyung <dokyung9@gmail.com>
2014-06-02 17:45:08 +0900

    exhibitionInfo_post_ok.php when file uploads, not alert.

--- www/exhibitionInfo_post_ok.php
+++ www/exhibitionInfo_post_ok.php
@@ -37,21 +37,19 @@ if($mode == 'correct') {
 			($_FILES["map"]["type"] !=  "image/jpg") &&
 			($_FILES["map"]["type"] != "image/jpeg") ) {
 			$isError = "'파일형식을 .gif 또는 .png 또는 .jpg 또는 .jpeg 형식으로 올리시오.'";
-		}  else if($_FILES["map"]["size"]>512000) {
+		} else if($_FILES["map"]["size"]>512000) {
 			$isError = "'파일크기를 500KB 미만으로 올리시오.'";
+		} else if($sizeInfo[0]>500 || $sizeInfo[1]>800){
+
+			$isError="'파일크기를 500*800 이내로 하시오.'";
 		} else {
-			$sizeInfo = getimagesize($_FILES["map"]["tmp_name"]);
+			$isError="success";
+			move_uploaded_file($_FILES["map"]["tmp_name"], $map);
 
-			if($sizeInfo[0]>500 || $sizeInfo[1]>800) { 
-				$isError="'파일크기를 500*800 이내로 하시오.'";
-			} else {
-
-				move_uploaded_file($_FILES["map"]["tmp_name"], $map);
-			}
 		}
 	}
 // echo $isError;
-	if($isError ==null) {
+	if($isError =='success') {
 		echo '<script> alert("사진을 성공적으로 업로드하였습니다.");</script>';
 	}else {
 		?> 
